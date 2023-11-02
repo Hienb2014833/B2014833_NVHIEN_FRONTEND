6b3b477954e3efbd8e9c93435a42cb938f3f7a65
Hienb2014833 <hienb2014833@student.ctu.edu.vn>
2023-11-02 10:21:21 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,13 @@ export default defineConfig({
     }
   }
 
-  server:{
-    port:3001,
+  server: {
+    port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+    }
   },
 });
